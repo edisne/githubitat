fc691a06dec8ef21b739c13c1889d3feaba2a007
Edis Neziric <e.neziric@3dqr.de>
2024-02-29 20:25:53 +0100

    Changed github api token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  token: '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',
+  token: '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',
   baseUrl: 'https://api.github.com',
 };
